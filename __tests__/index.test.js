daa1153739b0e499d76c62ef9a45b1922c36c912
Jeffrey Gilbert <jgilber@costco.com>
2020-02-10 20:14:01 -0800

    Awqdded a few more tests.  basic exercising of jest

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { Guber } = require('../data/index');
-const { validate, setRules, reloadRules } = require('../lib/index');
+const { validate, setRules, getRules, reloadRules } = require('../lib/index');
 const fs = require('fs');
 
 afterEach(() => {
@@ -17,5 +17,37 @@ describe('Tesing rules engine', () => {
     await validate(testData);
     expect(testData.user.mood).toBe('great');
     expect(testData.goWalking).toBeTruthy();    
+  });
+  it('Testing setting rules and getting rules', () => {
+    const newRules = ['./__tests__/rules/sample1a.json', './__tests__/rules/sample1b.json'];
+    setRules(newRules);
+    const rules = getRules();
+    expect(newRules).toEqual(
+      expect.arrayContaining(rules),
+    );
+  });
+  it('Testing setting rules.  But with wrong datatype', () => {
+    const newRules = {};
+    setRules(newRules);
+    const rules = getRules();
+    expect.arrayContaining(['./rules/sampleRule.json']);
+  });
+  it('Testing setting rules with an empty array', () => {
+    const newRules = [];
+    setRules(newRules);
+    const rules = getRules();
+    expect.arrayContaining(['./rules/sampleRule.json']);
+  });
+  it('Testing bad rules.  No file exist', () => {
+    const newRules = ['./__tests__/rules/sample1a.json', './__tests__/rules/sample1d.json'];
+    expect(() => {
+      setRules(newRules);
+    }).toThrow();  
+  });
+  it('Testing bad rules.  Not valid Javascript', () => {
+    const newRules = ['./__tests__/rules/sample1a.json', './__tests__/rules/badSample.json'];
+    expect(() => {
+      setRules(newRules);
+    }).toThrow();  
   });
 });
